559c4eec0d9437231f19c9fe45c668a18ce9a45d
DmitryPateew <isildor.008@mail.ru>
2022-04-17 17:48:08 +0200

    remove unused function

--- src/services/services.js
+++ src/services/services.js
@@ -7,10 +7,6 @@ export const formatTime = (seconds) => {
         : moment(seconds * 1000).format('hh:mm:ss');
 }
 
-export const getProgressWidth = (currentTime, duration) => {
-    return (currentTime / duration) * 100;
-}
-
 export const enableFullScreen = (element) => {
     const {userAgent} = navigator;
 
